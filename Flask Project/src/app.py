7bed70936f34fb2fb78e3f2320a96cc4a649ba98
sensoum <ssen.mailbox@gmail.com>
2018-12-18 02:18:13 +0530

    refactoring

--- Flask Project/src/app.py
+++ Flask Project/src/app.py
@@ -1,16 +1,19 @@
-import os, pandas as pd, matplotlib.pyplot as plt
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-target = os.path.join(APP_ROOT, 'file_docs\\')
+target = os.path.join(APP_ROOT, '..\\file_docs\\')
+
 
 def count_plt(parm):
-    df = pd.read_excel(r'file_docs\target_file.xlsx')
+    df = pd.read_excel(r'..\file_docs\target_file.xlsx')
     fig = df.groupby(parm).count()['Employee Code'].plot.bar().get_figure()
-    fig.savefig(r"static\output.png", bbox_inches='tight')
+    fig.savefig(r"..\static\output.png", bbox_inches='tight')
 
 
 def col_list(file):
@@ -30,9 +33,9 @@ def upload():
         if not os.path.isdir(target):
             os.mkdir(target)
         file = request.files.get("file")
-        file.save('\\'.join([target,'target_file.xlsx']))
+        file.save('\\'.join([target, 'target_file.xlsx']))
         labels = col_list(file)
-        return render_template("index.html",labels=labels)
+        return render_template("index.html", labels=labels)
     elif request.method == 'POST':
         print(2)
         plot = request.form['plot']
@@ -41,10 +44,10 @@ def upload():
             parm = request.form['parm']
             print(parm)
             count_plt(parm)
-            return render_template("output.html", user_image=r'static\output.png')
+            return render_template("output.html", user_image=r'..\static\output.png')
         else:
             return 'Plot not available'
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.91',port='98',debug=True)
+    app.run(host='127.0.0.91', port='98', debug=True)
